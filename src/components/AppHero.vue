2ee5ab30c83a6361ee2256fb007ec214735ece4a
DarioRossi99 <rssdra99@gmail.com>
2022-12-01 15:17:54 +0100

    bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+
+ export default{
+
+ }
+
+</script>
+
+<template>
+  <p>spero funzioni AppHero</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+
+ export default{
+
+ }
+
+</script>
+
+<template>
+  <h1>La mia prima App vite</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
